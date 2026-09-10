f54ed14c4dfbe004eb1031da0cec6b1da6da91bd
Brandon Quintero <121878425+branr9@users.noreply.github.com>
2026-09-10 11:17:46 -0500

    configuracion de certificado

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import { provideRouter } from '@angular/router';
+
+import {
+  provideHttpClient,
+  withInterceptors,
+  withFetch
+} from '@angular/common/http';
+
 import { registerLocaleData } from '@angular/common';
 import localeEsCO from '@angular/common/locales/es-CO';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import {
+  provideClientHydration,
+  withEventReplay
+} from '@angular/platform-browser';
+
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 registerLocaleData(localeEsCO, 'es-CO');
@@ -13,9 +29,19 @@ registerLocaleData(localeEsCO, 'es-CO');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation()),
-    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
+
+    provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+      withFetch()
+    ),
+
     provideClientHydration(withEventReplay()),
-    { provide: LOCALE_ID, useValue: 'es-CO' }
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CO'
+    }
   ]
 };
